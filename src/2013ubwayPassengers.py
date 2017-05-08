7b885161ffa4341a04a6a64674279a9cb3398fdc
heuisu <heesu0429@naver.com>
2017-05-08 12:09:39 +0900

    Add files via upload

---
+++ src/2013ubwayPassengers.py
@@ -0,0 +1,19 @@
+import os
+import requests
+_url = 'http://openAPI.seoul.go.kr:8088'
+_key="4f654b6a4c6865653531624c445045"
+_type='xml'
+_service='CardSubwayStatisticsService'
+_start_index=1
+_end_index=5
+_use_mon='201306'
+_maxIter=2
+_iter=0
+while _iter<_maxIter:
+    _api=os.path.join(_url,_key,_type,_service,str(_start_index),str(_end_index),_use_mon)
+    print _api
+    #response = requests.get(_api).text
+    #print response
+    _start_index+=5
+    _end_index+=5
+    _iter+=1
